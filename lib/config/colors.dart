14b84bbb608df8026037e5e9227570cc6eb33b58
Abdusalom <abdusalom@gmail.com>
2024-11-14 12:20:42 +0500

    Removed the unnecessary lines of codes, the comments and added if needed explanation.

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -4,6 +4,6 @@ class ColorsClass {
   static Color dark = const Color(0xFF040D12);
   static Color milk = const Color(0xFFCCCCCC);
   static Color lightBlue = const Color(0xff386fa4);
-  static Color darkRed = const Color(0xFF9A031E);
+  static Color darkRed = const Color(0xFF92140c);
   static Color yellow = Colors.yellow;
 }

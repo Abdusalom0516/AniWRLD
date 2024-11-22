fe24257935db4c4954d767555420c126f5489881
Abdusalom <abdusalom@gmail.com>
2024-11-22 20:18:27 +0500

    Removed Special Scroll Sqweeezing effect from Search Screen.

--- lib/providers/details_data.dart
+++ lib/providers/details_data.dart
@@ -5,9 +5,11 @@ import 'package:flutter/material.dart';
 class DetailsData extends ChangeNotifier {
   List<String> listOfDetailsImg = [
     "https://i.pinimg.com/736x/c9/7b/cb/c97bcb1c7070e3a5ed0b45d862e094c3.jpg",
+    "https://i.pinimg.com/736x/25/b7/96/25b796129bdf395070d51c6f84179d3d.jpg",
     "https://i.pinimg.com/736x/2b/a9/97/2ba9974e31997b9058f9cda8d4cc4ab4.jpg",
     "https://i.pinimg.com/736x/9a/30/1f/9a301fa6a41ba85606cdda378b4c4d92.jpg",
-    "https://i.pinimg.com/474x/d2/a8/bf/d2a8bf3fc1ba2d1bc27eab933fe0ec3d.jpg"
+    "https://i.pinimg.com/474x/d2/a8/bf/d2a8bf3fc1ba2d1bc27eab933fe0ec3d.jpg",
+    "https://i.pinimg.com/736x/8d/30/86/8d30863273af5ddd8ce50bdd42310b63.jpg"
   ];
 
    int index = 0;
